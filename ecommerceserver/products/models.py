83a76f8138580d027209c82009639079cb575e43
Edgar018 <edgar_belandria010@hotmail.com>
2022-06-27 15:11:56 -0400

    feat: define product table

---
+++ ecommerceserver/products/models.py
@@ -0,0 +1,9 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
